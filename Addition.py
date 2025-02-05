430cc2069c1324a66f86c8ef942cfec9b30835b6
Uday <puday0730@gmail.com>
2025-02-05 12:56:15 +0530

    modified code

--- Addition.py
+++ Addition.py
@@ -1,4 +1,4 @@
-def greet(name):
+idef greet(name):
     """Function to greet a user."""
     return f"Hello, {name}! Welcome to Git and GitHub learning."
 
@@ -7,9 +7,9 @@ def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
+    user_name = input("Enter your name: Uday ")
     print(greet(user_name))
 
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
+    num1 = int(input("Enter first number: 20"))
+    num2 = int(input("Enter second number: 30"))
     print(f"Sum: {add_numbers(num1, num2)}")
